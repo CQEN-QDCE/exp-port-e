9e558c334ed4cae6ebdfc32e16e01fef1ff76dc4
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-05-17 09:45:16 -0400

    [func] change la reférence du template à utiliser
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- openshift/templates/verifier-physique/01-DeploymentConfigAgent.sh
+++ openshift/templates/verifier-physique/01-DeploymentConfigAgent.sh
@@ -1,1 +1,1 @@
-oc process -f ../../../../aries-toolkit/openshift/templates/consommateur/aries-consommateur.yaml --param-file=./agent/aries-consommateur.candy-dev.params.env | oc apply -f -
+oc process -f ./aries-consommateur.yaml --param-file=./agent/aries-consommateur.candy-dev.params.env | oc apply -f -
