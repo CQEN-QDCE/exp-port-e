e4b437e906030913177cdf6bf5572d833d681f66
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-06-09 09:52:06 -0400

    [func] ajouter les methodes d'action du relay
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- VerifierPhysique/firmware/Port-e-invitation/Port-e-invitation.ino
+++ VerifierPhysique/firmware/Port-e-invitation/Port-e-invitation.ino
@@ -15,12 +15,10 @@
 //  Wifi Libs
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+#include <ESP8266WiFi.h>
 
 // SerialComms Lib 
 #include <SoftwareSerial.h>
-
-// CRC32 Lib
-#include <CRC32.h>
 
 // -----------------------------
 // Fichiers header personnalisés
@@ -32,34 +30,41 @@
 // Variables globales 
 // -----------------------------
 
-// Client HTTPS
-WiFiClientSecure client;
+// Clients HTTPS
+WiFiClientSecure client;      // Client comms securitaire avec openshift
+WiFiClient       clientWeb;   // Client du web server
+
+// Set web server port number to 80
+WiFiServer server(80);
 
 // État de l'adresse: 
 //    si l'adresse n'existe pas ou est utilisé, son état est 0
 //    sinon, si l'adresse est généré et pas encore consommé, son état est 1 
 int etatAdresse;
 
+
 // Créé une porte serial virtuale aux pins digitaux 6(RX) et 7(TX)
 // Dans le cas de NodeMCU, TX=GPIO5=D1; RX=GPIO4=D2
 // Alors, connecter le pin D2 à RX, et D1 à TX; dans le code, référencer par les # gpio (4, 5)
 SoftwareSerial porteSerial(4, 5);
 
-CRC32 crc; 
+// Connecté au pin D4, alors gpio #2
+int pinRelay = 2; 
 
 /**
  * 
  */
 void setup(){
-    delay(DELAY_M);             // Serait il necessaire? 
+    //delay(3000);   // Serait il necessaire? 
     
     Serial.begin(115200);    // Baud plus vite pour l'application
     porteSerial.begin(9600); // Baud plus lent pour la communication serial 
 
     Serial.println("[WIFI] Setup de l'application..."); 
 
-    // Initialiser le CRC 
-    crc.reset();
+    // configure le pin du relay pour faire sortie
+    pinMode(pinRelay, OUTPUT); 
+    digitalWrite(pinRelay, HIGH);
 
     setupWifi();
 
@@ -77,33 +82,8 @@ void setup(){
 /**
  * 
  */
-void loop(){
-    Serial.println("[WIFI] Loop... ");
-
-    if(porteSerial.available() > 0){
-        String msg = porteSerial.readStringUntil('\n');
-
-        // Si le message reçu est 0x20 (générer nouveau) on génére l'addresse. 
-        // Sinon, on continue le loop.
-        Serial.print("[WIFI] Signal reçu: ");
-        Serial.println(SIG_GEN);
-        if(msg == SIG_GEN){
-            Serial.println("[WIFI] Adresse non disponible. Requisition d'un nouvel adresse en cours..."); 
-            String adresse = generer();
-            serialComm(adresse); 
-            delay(DELAY_L);
-        } else {
-            Serial.println("[WIFI] Pas le signal correct."); 
-        }
-    }
-    delay(DELAY_M);     // réconsiderer le delay ici. 
-}
-
-/**
- * 
- */
 void setupWifi(){
-    Serial.println("[WIFI] Setup WIFI");
+    Serial.println("[WIFI]  Setup WIFI");
 
     // -----------------------------
     // Configuration du WiFi 
@@ -130,12 +110,85 @@ void setupWifi(){
     IPAddress ip = WiFi.localIP();
     Serial.print("[WIFI] ");
     Serial.println(ip);
+    
+    // Initialise le web server local
+    server.begin();
+    
+    // Si vous n'avez pas besoin de checker le fingerprint
+    client.setInsecure();
 
-    // Si vous n'avez pas besoin de checker le fingerprint, décommenter la ligne suivante
-    // client.setInsecure();
+    // Sinon si vous voulez checker le fingerprint
+    // client.setFingerprint(HOST_FINGERPRINT);
+}
 
-    // Si vous voulez checker le fingerprint
-    client.setFingerprint(HOST_FINGERPRINT);
+/**
+ * 
+ */
+void loop(){
+    
+    Serial.println("[WIFI] Loop... ");
+
+    
+    clientWeb = server.available();
+    if(clientWeb){
+        Serial.println("Nouveau client connecté"); 
+
+        // Attendre requete du client
+        while(clientWeb.connected()){
+          if(clientWeb.available()){
+            // Lire requete du client
+            String request = clientWeb.readStringUntil('\r');
+            clientWeb.flush(); 
+            Serial.println(request);
+
+            // Verificar se a solicitação é um GET
+        if (request.indexOf("GET") != -1) {
+          Serial.println("C");
+          // Verificar se a solicitação contém um caminho específico, por exemplo, "/ligar"
+          if (request.indexOf("/acces") != -1) {
+            Serial.println("D");
+            // Ativar o pino 12
+            digitalWrite(12, HIGH); 
+            Serial.println("Appel a la fonction d'acces autorisé");
+            acces();
+            sendResponse(clientWeb, 200, "OK"); // Enviar resposta para o cliente
+          } else if (request.indexOf("/refus") != -1) {
+            Serial.println("E");
+            // Desativar o pino 12
+            digitalWrite(12, LOW);
+            refus();
+            sendResponse(clientWeb, 200, "OK"); // Enviar resposta para o cliente
+          } else {
+            // Caminho desconhecido
+            Serial.println("F");
+            Serial.println("Oups, problemas em vista"); 
+            sendResponse(clientWeb, 404, "Not Found"); // Enviar resposta para o cliente
+          }
+        }
+        // Encerrar a conexão com o cliente
+        clientWeb.stop();
+        Serial.println("Cliente desconectado");
+            }
+          }  
+        }
+    
+    if(porteSerial.available() > 0){
+        String msg = porteSerial.readStringUntil('\n');
+
+        // Si le message reçu est 0x20 (générer nouveau) on génére l'addresse. 
+        // Sinon, on continue le loop.
+        Serial.print("[WIFI] Signal reçu: ");
+        Serial.println(SIG_GEN);
+        if(msg == SIG_GEN){
+            Serial.println("[WIFI] Adresse non disponible. Requisition d'un nouvel adresse en cours..."); 
+            String adresse = generer();
+            serialComm(adresse); 
+            delay(DELAY_L);
+        } else {
+            Serial.println("[WIFI] Pas le signal correct."); 
+        }
+    }
+    delay(DELAY_S);     // réconsiderer le delay ici. 
 }
 
 /**
@@ -228,4 +281,42 @@ void serialFlush(){
     while(porteSerial.available() > 0) {
         char t = porteSerial.read();
     }
+}
+
+
+void sendResponse(WiFiClient client, int statusCode, const char* message) {
+  // Enviar linha de status HTTP
+  client.println("HTTP/1.1 " + String(statusCode) + " OK");
+  // Enviar cabeçalho de conteúdo
+  client.println("Content-Type: text/plain");
+  // Enviar linha em branco para indicar o fim do cabeçalho
+  client.println();
+  // Enviar a mensagem de resposta
+  client.println(message);
+}
+
+/**
+ * Ouvre la porte, selon les accès autorisés par l'identité numéerique. 
+ * // Normally Open configuration, send LOW signal to let current flow
+  // (if you're usong Normally Closed configuration send HIGH signal)
+ */
+void acces(){
+  Serial.println("Accès autorisé. Welcome in...");
+  digitalWrite(pinRelay, LOW); 
+  delay(5000); 
+  digitalWrite(pinRelay, HIGH);   
+  Serial.println("Fin de la methode acces");
+}
+
+/**
+ * Refuse l'acces 
+ *   // Normally Open configuration, send HIGH signal stop current flow
+  // (if you're usong Normally Closed configuration send LOW signal)
+ */
+void refus(){
+  Serial.println("Accès refusé. Go away...");
+
+  /*digitalWrite(pin, HIGH); 
+  delay(5000); 
+  digitalWrite(pinLed, LOW); */
 }
