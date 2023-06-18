1aca0d764ed859760d5c042a5f0514e2aaee13c7
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-06-17 23:22:51 -0400

    [func] amelioration du script; reception de parametre
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- VerifierPhysique/firmware/Port-e-invitation/start_ngrok.sh
+++ VerifierPhysique/firmware/Port-e-invitation/start_ngrok.sh
@@ -1,1 +1,16 @@
-ngrok http --host-header=rewrite 172.20.10.10:80
+ #!/bin/bash
+
+# Script qui lance le tunnel ngrok pour le serveur 
+# qui répond à l'adresse IP en paramètre. 
+
+# Variable reçoit l'adresse IP
+IP_ADDR=$1
+
+# Vérifie si l'adresse IP est innexistant; si c'est le cas, fournir une erreur
+if [ -z ${IP_ADDR} ]; then
+    echo "Erreur de parametrisation" 
+    echo "Utilisation: sh start_ngrok.sh <ip address>" 
+else 
+    echo "Démarrer le tunnel ngrok pour le serveur a l'adresse IP: [${IP_ADDR}]" 
+    ngrok http --host-header=rewrite ${IP-ADDR}:80
+fi
